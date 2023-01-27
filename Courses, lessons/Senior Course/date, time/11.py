aec98bc8cac94ef7f5a51afa590df7afc113e47f
Zavesnickiy Nikita <pakito.nikita95@gmail.com>
2023-01-27 23:26:55 +0300

    send files

--- Courses, lessons/Senior Course/date, time/11.py
+++ Courses, lessons/Senior Course/date, time/11.py
@@ -5,3 +5,10 @@ andrew = date(1992, 8, 24)
 print(andrew.strftime('%Y-%m'))   # выводим дату в формате YYYY-MM
 print(andrew.strftime('%B (%Y)'))   # выводим дату в формате month_name (YYYY)
 print(andrew.strftime('%Y-%j'))   # выводим дату в формате YYYY-day_number
+
+a = []
+for i in range(1, 101):
+    if i % 2 ==0:
+        a.append(i)
+
+print(sum(a))
