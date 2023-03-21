2d9745e0b93349634645cb819a64cc56310f28d5
Zavesnickiy Nikita <pakito.nikita95@gmail.com>
2023-03-22 00:13:21 +0300

    send files

--- Courses, lessons/Senior Course/date, time/32.py
+++ Courses, lessons/Senior Course/date, time/32.py
@@ -3,6 +3,6 @@ from datetime import date, timedelta
 today = date(2021, 11, 4)
 birthday = date(2022, 10, 6)
 
-days = timedelta.total_seconds(today - birthday) // 86400
+days = (birthday - today).days
 
 print(days)
