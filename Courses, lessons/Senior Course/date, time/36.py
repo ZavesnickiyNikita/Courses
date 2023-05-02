2ce1075ca5ee241324c0db017565169acdfa4f0d
Nikita <pakito.nikita95@gmail.com>
2023-05-02 11:24:43 +0300

    send files

--- Courses, lessons/Senior Course/date, time/36.py
+++ Courses, lessons/Senior Course/date, time/36.py
@@ -2,8 +2,14 @@ from datetime import *
 
 
 def num_of_sundays(year):
-    year = datetime.year(year)
-    return year // 7
-
+    current_year = datetime.strptime(str(year), '%Y')
+    next_year = current_year + date.year(1)
+    return next_year
+    # count = 0
+    # for i in range(current_year):
+    #     if i == datetime.weekday(6):
+    #         count += 1
+    # return count
+    
 
 print(num_of_sundays(2021))
